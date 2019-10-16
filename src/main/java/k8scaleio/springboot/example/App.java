a19af74c9a169b27d49507afe9f5a829e0fdd35c
interviewparrot <amrita@interviewparrot.com>
2019-10-16 19:44:22 -0400

    First commit

---
+++ src/main/java/k8scaleio/springboot/example/App.java
@@ -0,0 +1,15 @@
+package k8scaleio.springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author k8scaleio created on 13-Sep-2019
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(App.class, args);
+    }
+}
